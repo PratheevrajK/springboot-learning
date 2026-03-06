aaca33187633cc4d95422e04bc6364f1c9ac76f2
PratheevrajK <pratheevraj002@gmail.com>
2026-03-06 18:54:28 +0530

    Custom component scan

---
+++ 02-spring-core/src/main/java/com/luv2code/external/ExternalCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.external;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExternalCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Jog 10 rounds!";
+    }
+}
--- 02-spring-core/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-core/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,8 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
